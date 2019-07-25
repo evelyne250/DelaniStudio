f99a742a45fa8007c88ab00c3fbdd01ec778325c
evelyne250 <uevelyne44@gmail.com>
2019-07-25 17:58:46 +0200

    used jquery to hide and show images and paragraphs

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $(".design, .call").click(function() {
+      $("#design").toggle();
+      $(".call").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".develop, .call1").click(function() {
+        $("#develop").toggle();
+      $(".call1").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".product, .call2").click(function() {
+        $("#product").toggle();
+      $(".call2").toggle();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".work").hover(function(){
+      $(this).css("background-color", "yellow");
+    });
+});
