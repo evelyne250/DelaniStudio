04e245dfd17f168effd3fc4a171ec9746884ad64
evelyne250 <uevelyne44@gmail.com>
2019-07-26 16:22:40 +0200

    added hover over the icons

--- script.js
+++ script.js
@@ -16,5 +16,113 @@ $(document).ready(function() {
       $(".call2").toggle();
     });
   });
+// $(.work).hide();
+// $(#work).animate({
+//   opacity:1
+// })
+//   $(document).hover(function() {
+//     $(this).stop().animate({opacity:.4},200);
+//     $('.work').fadein();
+//   }, function(){
+//     $(this).stop().animate({opacity:1},500)
+//     $('.work').fadeout();
+//   });
 
- 
+// $(document).ready(function() {
+// $("#work").hover(function()
+// // css("background-color", "grey", "background-size", "cover");
+// //   }, function(){
+//   $(this).css("background-color", "pink");
+// }, function(){
+//   $(this).css("background-color", "pink");
+// });
+// });
+
+$(document).ready(function(){
+  $(".wor").hide();
+  $("#work").css({
+   opacity:1
+ });
+  $("#wo").hover(function(){
+    $(this).stop().css({opacity:.4});
+    $(".wor").fadeIn();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(".wor").fadeOut();
+  });
+});
+$(document).ready(function(){
+  $("#work1").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work2").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work3").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work4").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work5").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work6").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+$(document).ready(function(){
+  $("#work7").hover(function(){
+    $(this).stop().css({opacity:.3});
+    $(this).fadein();
+    }, function(){
+      $(this).stop().css({opacity:1})
+    $(this).fadeout();
+  });
+});
+// 
+//     alert("we have received your response,  Thank you for reaching out to us.");
+//   
+// });
+$(document).ready(function(){
+$("form").submit(function(){
+var nameIs= document.getElementById("user");
+var email= document.getElementById("user1");
+var comment= document.getElementById("user2");
+alert('Hi  ' +  nameIs.value + ' Thank you for reaching out to us.');
+});
+});
